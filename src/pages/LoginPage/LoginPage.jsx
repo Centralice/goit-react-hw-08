029deccdcc6209b8926a2e457dc9e39058ee54e1
Centralice <108938330+Centralice@users.noreply.github.com>
2024-09-19 22:37:31 +0200

    FORMS

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,13 +1,41 @@
+import { Field, Formik, Form, ErrorMessage } from "formik";
 import s from "./LoginPage.module.css";
+import { useId } from "react";
+import * as Yup from "yup";
 
 const LoginPage = () => {
+  function login(values, actions) {
+    console.log(values);
+    actions.resetForm();
+  }
+  const id = useId();
+  const loginSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(3, "Too Short!")
+      .max(30, "Too Long!")
+      .required("Required"),
+    password: Yup.string()
+      .min(8, "Too Short!")
+      .max(30, "Too Long!")
+      .required("Required"),
+  });
   return (
     <div>
-      <form>
-        <input></input>
-        <input></input>
-        <button type="submit">Sign In</button>
-      </form>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={login}
+        validationSchema={loginSchema}
+      >
+        <Form>
+          <label htmlFor={`${id}-username`}>Username</label>
+          <Field type="text" name="username" id={`${id}-username`}></Field>
+          <ErrorMessage name="username"></ErrorMessage>
+          <label htmlFor={`${id}-password`}>Password</label>
+          <Field type="password" name="password" id={`${id}-password`}></Field>
+          <ErrorMessage name="password"></ErrorMessage>
+          <button type="submit">Sign In</button>
+        </Form>
+      </Formik>
     </div>
   );
 };
