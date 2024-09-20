d2571f44b6883008b8e38f3d4d993840649f2860
Centralice <108938330+Centralice@users.noreply.github.com>
2024-09-20 21:32:03 +0200

    navlinks added and styled

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,11 +1,26 @@
-import { Link } from 'react-router-dom';
-import s from './AuthNav.module.css'
+import { NavLink } from "react-router-dom";
+import s from "./AuthNav.module.css";
+import clsx from "clsx";
 
 const AuthNav = () => {
   return (
     <div className={s.btns}>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Sign up</Link>
+      <NavLink
+        to="/login"
+        className={(props) => {
+          return clsx(s.link, props.isActive && s.active);
+        }}
+      >
+        Login
+      </NavLink>
+      <NavLink
+        to="/register"
+        className={(props) => {
+          return clsx(s.link, props.isActive && s.active);
+        }}
+      >
+        Sign up
+      </NavLink>
     </div>
   );
 };
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,26 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import s from "./Navigation.module.css";
+import clsx from "clsx";
 
 const Navigation = () => {
   return (
     <div className={s.navigation}>
-      <Link to="/">Home</Link>
-      <Link to="/contacts">Contacts</Link>
+      <NavLink
+        to="/"
+        className={(props) => {
+          return clsx(s.link, props.isActive && s.active);
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/contacts"
+        className={(props) => {
+          return clsx(s.link, props.isActive && s.active);
+        }}
+      >
+        Contacts
+      </NavLink>
     </div>
   );
 };
