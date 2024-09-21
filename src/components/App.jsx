de6c5562bcb61b88aca918c22c79ae6482ca00db
Centralice <108938330+Centralice@users.noreply.github.com>
2024-09-21 20:09:40 +0200

    lazy and suspense added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,32 @@
 import { Route, Routes } from "react-router-dom";
 import AppBar from "./AppBar/AppBar";
-import HomePage from "../pages/HomePage/HomePage";
-import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
-import RegisterPage from "../pages/RegisterPage/RegisterPage";
-import LoginPage from "../pages/LoginPage/LoginPage";
-import ContactsPage from "../pages/ContactsPage/ContactsPage";
+// import HomePage from "../pages/HomePage/HomePage";
+// import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+// import RegisterPage from "../pages/RegisterPage/RegisterPage";
+// import LoginPage from "../pages/LoginPage/LoginPage";
+// import ContactsPage from "../pages/ContactsPage/ContactsPage";
+import { lazy, Suspense } from "react";
+import Loader from "./Loader/Loader";
+
+const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
+const NotFoundPage = lazy(() => import("../pages/NotFoundPage/NotFoundPage"));
+const RegisterPage = lazy(() => import("../pages/RegisterPage/RegisterPage"));
+const LoginPage = lazy(() => import("../pages/LoginPage/LoginPage"));
+const ContactsPage = lazy(() => import("../pages/ContactsPage/ContactsPage"));
 
 const App = () => {
   return (
     <div>
       <AppBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
