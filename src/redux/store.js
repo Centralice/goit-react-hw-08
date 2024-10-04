820b6a74de5ca54d916ba123a2955975ccf023b7
Centralice <108938330+Centralice@users.noreply.github.com>
2024-10-04 22:44:21 +0200

    work in progress

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contacts/slice";
 import filtersReducer from "./filters/slice";
 import authReducer from "./auth/slice";
+
+const persistedAuthReducer = persistReducer(
+  {
+    key: "root",
+    storage,
+    whitelist: ["token"],
+  },
+  authReducer
+);
 
 export const store = configureStore({
   reducer: {
@@ -10,3 +21,5 @@ export const store = configureStore({
     filters: filtersReducer,
   },
 });
+
+export const persistor = persistStore(store);
