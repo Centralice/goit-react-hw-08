6c3207bfe08661e73183b7fbf7077debeb1ac6f0
Centralice <108938330+Centralice@users.noreply.github.com>
2024-10-06 20:22:42 +0200

    main done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { refreshUser } from "../redux/auth/operations";
 import { selectIsRefreshing } from "../redux/auth/selectors";
 import RestrictedRoute from "./RestrictedRoute";
+import PrivateRoute from "./PrivateRoute";
 
 const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
 const NotFoundPage = lazy(() => import("../pages/NotFoundPage/NotFoundPage"));
@@ -36,7 +37,10 @@ const App = () => {
             path="/login"
             element={<RestrictedRoute component={<LoginPage />} />}
           />
-          <Route path="/contacts" element={<ContactsPage />} />
+          <Route
+            path="/contacts"
+            element={<PrivateRoute component={<ContactsPage />} />}
+          />
         </Routes>
       </Suspense>
     </div>
